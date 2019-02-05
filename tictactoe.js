f7f3fc88cdfb15137c8b05c825039f9b23744349
MelaatiJ <fathimajayah876@gmail.com>
2019-02-05 16:52:59 -0600

    classwork

---
+++ tictactoe.js
@@ -0,0 +1,71 @@
+var gameSpaces = document.querySelectorAll("td");
+//calling all my td
+var currentPlayer = 1;
+//setting it to 1
+var resetButton = document.getElementsByTagName("button");
+//calling my reset button using get elements by tag name
+
+resetButton[0].onclick = resetButtonFunction;
+//when clicked it will call my function resetebuttonfunction
+function resetButtonFunction(e)
+{
+    for(var t=0; t<gameSpaces.length;t++)
+    {//this will iterate through each one and make it equal into an empty string when the reset button is clicked
+        gameSpaces[t].innerHTML= "";
+    }
+}
+function clickedSquare(e)
+{
+    console.log("clickedsquare");
+    if (currentPlayer ===1 && e.target.innerHTML==="" )
+    {
+        e.target.innerHTML ="X";
+        currentPlayer=2;
+    }
+    else if(e.target.innerHTML==="")
+    {
+        e.target.innerHTML ="O";
+        currentPlayer=1;
+
+    }
+
+    //call our check for winner
+    checkForWinner();
+}
+
+for (var i = 0; i < gameSpaces.length; i++) {
+    gameSpaces[i].addEventListener('click', clickedSquare);
+}
+
+
+//function to check for a winner
+function checkForWinner()
+{
+    console.log("hello");
+    if((gameSpaces[0].innerText=== gameSpaces[1].innerText &&gameSpaces[1].innerText === gameSpaces[2].innerText && gameSpaces[2].innerText==="X")
+        ||(gameSpaces[3].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[5].innerText && gameSpaces[5].innerText==="X")
+        ||(gameSpaces[6].innerText=== gameSpaces[7].innerText &&gameSpaces[7].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="X")
+        ||(gameSpaces[0].innerText=== gameSpaces[3].innerText &&gameSpaces[3].innerText === gameSpaces[6].innerText && gameSpaces[6].innerText==="X")
+        ||(gameSpaces[1].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[7].innerText && gameSpaces[7].innerText==="X")
+        ||(gameSpaces[2].innerText=== gameSpaces[5].innerText &&gameSpaces[5].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="X")
+        ||(gameSpaces[0].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="X")
+        ||(gameSpaces[2].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[6].innerText && gameSpaces[6].innerText==="X"))
+    {
+        alert("X wins");
+        return false;
+    }
+    else if((gameSpaces[0].innerText=== gameSpaces[1].innerText &&gameSpaces[1].innerText === gameSpaces[2].innerText && gameSpaces[2].innerText==="O")
+        ||(gameSpaces[3].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[5].innerText && gameSpaces[5].innerText==="O")
+        ||(gameSpaces[6].innerText=== gameSpaces[7].innerText &&gameSpaces[7].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="O")
+        ||(gameSpaces[0].innerText=== gameSpaces[3].innerText &&gameSpaces[3].innerText === gameSpaces[6].innerText && gameSpaces[6].innerText==="O")
+        ||(gameSpaces[1].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[7].innerText && gameSpaces[7].innerText==="O")
+        ||(gameSpaces[2].innerText=== gameSpaces[5].innerText &&gameSpaces[5].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="O")
+        ||(gameSpaces[0].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[8].innerText && gameSpaces[8].innerText==="O")
+        ||(gameSpaces[2].innerText=== gameSpaces[4].innerText &&gameSpaces[4].innerText === gameSpaces[6].innerText && gameSpaces[6].innerText==="O"))
+    {
+        alert("O wins");
+        return false;
+    }
+
+}
+
